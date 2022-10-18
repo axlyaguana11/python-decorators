233be3f91d0f49de8daffb692e4df0fafeb7476d
axlyaguana11 <axl9809@gmail.com>
2022-10-18 14:09:13 -0500

    Multiple args file

--- decorators/multiple_args.py
+++ decorators/multiple_args.py
@@ -1,1 +1,15 @@
-#Arguments in 
+#Arguments in decorator and decorated function
+
+def multiply_result(num_3):
+    def inner_decorator(func):
+        def wrapper(arg1, arg2):
+            print(f'({arg1} + {arg2}) * {num_3}:')
+            return func(arg1, arg2) * num_3 
+        return wrapper
+    return inner_decorator
+
+@multiply_result(3)
+def sum_nums(num_1, num_2):
+    return num_1 + num_2
+
+print(sum_nums(1, 2))
